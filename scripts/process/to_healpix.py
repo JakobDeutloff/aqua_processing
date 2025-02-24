317af86eb143818004fdf76ed1a7f13bfc69995f
jakob.deutloff <jakob.deutloff@gmail.com>
2025-02-24 17:55:50 +0100

    Use most simple chunks for subssampling, 2d_hp variability analysis added

--- scripts/process/to_healpix.py
+++ scripts/process/to_healpix.py
@@ -6,7 +6,7 @@ import pandas as pd
 # %% load dataset
 ds = (
     xr.open_mfdataset(
-        "/work/bm1183/m301049/icon-mpim/experiments/jed0011/jed0011_atm_2d_19*.nc",
+        "/work/bm1183/m301049/icon-mpim-2K/experiments/jed0033/jed0033_atm_2d_19*.nc",
         chunks={"time": 1},
     )
     .pipe(merge_grid)
@@ -18,4 +18,4 @@ time_coarse = pd.date_range(start=ds.time.values[0], end=ds.time.values[-1], fre
 ds_coarse = ds.sel(time=time_coarse)
 
 # %% regrid to healpix
-to_healpix(ds_coarse, save_path="/work/bm1183/m301049/icon_hcap_data/control/production/jed0011_atm_2d_hp.nc")
+to_healpix(ds_coarse, save_path="/work/bm1183/m301049/icon_hcap_data/plus2K/production/jed0033_atm_2d_hp.nc")
