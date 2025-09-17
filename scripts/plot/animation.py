54ff727b41b8db8da043f04ba9f803706508d7eb
jakob.deutloff <jakob.deutloff@gmail.com>
2025-09-17 11:53:21 +0200

    moist layer stuff moved to src, animation completed

--- scripts/plot/animation.py
+++ scripts/plot/animation.py
@@ -6,62 +6,90 @@ import easygems.healpix as egh
 import numpy as np
 from src.grid_helpers import merge_grid
 from matplotlib.colors import LinearSegmentedColormap, LogNorm
-import matplotlib.animation as animation
 from concurrent.futures import ProcessPoolExecutor
 from tqdm import tqdm
+import os
 
 # %% load data
-ds = xr.open_dataset('/work/bm1183/m301049/icon_hcap_data/control/production/twp_hp.nc')
+ds = xr.open_dataset("/work/bm1183/m301049/icon_hcap_data/control/production/twp_hp.nc")
 ds.attrs = {
-    'grid_file_uri': 'http://icon-downloads.mpimet.mpg.de/grids/public/mpim/0015/icon_grid_0015_R02B09_G.nc'
+    "grid_file_uri": "http://icon-downloads.mpimet.mpg.de/grids/public/mpim/0015/icon_grid_0015_R02B09_G.nc"
 }
 ds = ds.pipe(merge_grid)
 
 
-# %% 
-transparent_white_cmap = LinearSegmentedColormap.from_list(
-    'transparent_white', [(1, 1, 1, 0), (1, 1, 1, 1)]
-)
+# %% make colormap that goes from steelblue to white without alpha channel
+background = (0, 0, 0)
+white_cmap = LinearSegmentedColormap.from_list("white", [background, "#FF7FBD"])
 
-#%% setup figure
-fig_height_in = 33.1
-fig_width_in = 46.8
-projection = ccrs.Mollweide()
+# %% setup figure
+fig_height_in = 14
+fig_width_in = 24.3
+projection = ccrs.Orthographic()
 
-fig, ax = plt.subplots(
-    figsize=(fig_width_in, fig_height_in), subplot_kw={"projection": projection}
-)
-fig.set_dpi(300)
-steelblue = '#07222e'
-ax.set_facecolor(steelblue)
-ax.spines['geo'].set_edgecolor(steelblue)
-fig.patch.set_facecolor(steelblue)
-ax.set_global()
-fig.subplots_adjust(left=0.03, right=0.97)
-_, _, nx, ny = np.array(ax.bbox.bounds, dtype=int)
-xlims = ax.get_xlim()
-ylims = ax.get_ylim()
+# %% Precompute all frames in parallel and save as PNGs
+output_dir = "/work/bm1183/m301049/animation/frames"
+os.makedirs(output_dir, exist_ok=True)
 
-def preprocess_frame(frame):
-    return egh.healpix_resample(
-        ds['twp'].isel(time=frame).values, xlims, ylims, nx, ny, ax.projection, 'nearest', nest=True
+
+def save_frame_png(frame):
+
+    # prepare
+    fig, ax = plt.subplots(
+        figsize=(fig_width_in, fig_height_in), subplot_kw={"projection": projection}
+    )
+    fig.set_dpi(300)
+
+    ax.set_facecolor(background)
+    ax.spines["geo"].set_edgecolor(background)
+    fig.patch.set_facecolor(background)
+    ax.set_global()
+    _, _, nx, ny = np.array(ax.bbox.bounds, dtype=int)
+    xlims = ax.get_xlim()
+    ylims = ax.get_ylim()
+
+    # subsample
+    arr = egh.healpix_resample(
+        ds["twp"].isel(time=frame).values,
+        xlims,
+        ylims,
+        nx,
+        ny,
+        ax.projection,
+        "nearest",
+        nest=True,
     )
 
-# %% Precompute all frames in parallel
-with ProcessPoolExecutor(max_workers=16) as executor:
-    frames = list(tqdm(executor.map(preprocess_frame, range(25))))
+    # plot
+    ax.imshow(
+        arr,
+        extent=xlims + ylims,
+        origin="lower",
+        cmap=white_cmap,
+        norm=LogNorm(5e-2, 1e1),
+    )
 
-#%% animate
-img = ax.imshow(frames[0], extent=xlims + ylims, origin="lower", cmap=transparent_white_cmap, norm=LogNorm(3e-2, 1e1))
+    # Tighten layout
+    fig.subplots_adjust(top=0.95, bottom=0.05)
 
-def update(frame):
-    img.set_data(frames[frame])
-    return (img,)
+    # save
+    fname = os.path.join(output_dir, f"frame_{frame:04d}.png")
+    fig.savefig(fname)
+    plt.close(fig)
+    return fname
 
-ani = animation.FuncAnimation(
-    fig, update, frames=len(frames), interval=40, blit=True
-)
+
+with ProcessPoolExecutor(max_workers=64) as executor:
+    list(tqdm(executor.map(save_frame_png, range(len(ds.time))), total=len(ds.time)))
 
 # %%
-ani.save("plots/animation.gif", fps=25)
+anim_path = "/work/bm1183/m301049/animation/animation_pink_5.mp4"
+if os.path.exists(anim_path):
+    os.remove(anim_path)
+
+os.system(
+    f"ffmpeg -framerate 25 -i /work/bm1183/m301049/animation/frames/frame_%04d.png -c:v libx264 -pix_fmt yuv420p {anim_path}"
+)
+
+
 # %%
--- scripts/plot/moist_layers/agg_itcz.py
+++ scripts/plot/moist_layers/agg_itcz.py
@@ -1,11 +1,12 @@
 # %%
 import xarray as xr
 import matplotlib.pyplot as plt
-import cartopy.crs as ccrs
 import numpy as np
 from tqdm import tqdm
-from src.grid_helpers import merge_grid, fix_time
 import datetime as dt
+from concurrent.futures import ProcessPoolExecutor
+from src.grid_helpers import merge_grid, fix_time
+from src.moist_layers import plot_prw_field, get_contour_length
 
 # mpl.use("WebAgg")  # Use WebAgg backend for interactive plotting
 
@@ -17,13 +18,13 @@ ds = ds.sel(time=(ds.time.dt.minute == 0) & (ds.time.dt.hour == 0))
 
 # %% check histogram
 fig, ax = plt.subplots(figsize=(8, 5))
-ds["prw"].isel(time=slice(80, 90)).sel(lat=slice(-20, 20)).plot.hist(
+ds["prw"].isel(time=slice(80, 90)).sel(lat=slice(-40, 40)).plot.hist(
     bins=np.arange(0, 60, 1),
     ax=ax,
     color="grey",
 )
 ax.spines[["top", "right"]].set_visible(False)
-fig.savefig("plots/moist_layers/prw_histogram_20_20.png", dpi=300, bbox_inches="tight")
+fig.savefig("plots/moist_layers/prw_histogram_40_40.png", dpi=300, bbox_inches="tight")
 
 # %% plot pr vs prw
 ds_2d = (
@@ -47,152 +48,86 @@ ax.spines[["top", "right"]].set_visible(False)
 ax.set_xlabel("Water Vapor Path / mm$")
 ax.set_ylabel("Precipitation / mm day$^{-1}$")
 ax.set_title("")
+ax.set_yscale('log')
 ax.set_ylim(0, 100)
-fig.savefig("plots/moist_layers/pr_vs_prw.png", dpi=300, bbox_inches="tight")
+fig.savefig("plots/moist_layers/pr_vs_prw_20_20.png", dpi=300, bbox_inches="tight")
 
-# %% plot precip histogram
-fig, ax = plt.subplots(figsize=(8, 5))
-ds_2d["pr"].isel(time=slice(80, 90)).where(
-    (ds_2d["clat"] < 20) & (ds_2d["clat"] > -20), drop=True
-).plot.hist(
-    bins=np.arange(0, 0.05, 0.01),
-    ax=ax,
-    color="grey",
+
+# %% calculate contourlength 30 mm
+def contour_length_for_time(t):
+    # Helper for parallel execution
+    return int(get_contour_length(ds["prw"].sel(time=t), cont=30))
+
+with ProcessPoolExecutor(max_workers=64) as executor:
+    contour_lenghts = list(tqdm(executor.map(contour_length_for_time, ds["time"].values), total=len(ds["time"])))
+
+contour_lenghts_30 = xr.DataArray(
+    contour_lenghts / np.max(contour_lenghts),
+    dims=["time"],
+    coords={"time": ds["prw"].time},
+    attrs={"long_name": "Contour length of 30 mm PRW"},
 )
-ax.spines[["top", "right"]].set_visible(False)
-ax.set_xlabel("Precipitation / kg m$^{-2}$ s$^{-1}$")
-ax.set_ylabel("Frequency")
-fig.savefig("plots/moist_layers/pr_histogram_20_20.png", dpi=300, bbox_inches="tight")
-# %%
-ds_plot = (
-    ds["prw"]
-    .sel(time="1979-07-11")
-    .sel(lon=slice(0, 360), lat=slice(-50, 50))
-    .isel(lon=slice(None, None, 10), lat=slice(None, None, 10))
-)
-pad = 2  # for a window of 5, pad by 2 on each side
-ds_plot = ds_plot.pad(lon=(pad, pad), mode="wrap")
-ds_plot = ds_plot.rolling(lon=5, lat=5, center=True).mean()
 
-fig, ax = plt.subplots(figsize=(10, 5), subplot_kw={"projection": ccrs.PlateCarree()})
-ds_plot.plot.contourf(
-    ax=ax,
-    transform=ccrs.PlateCarree(),
-    levels=np.arange(15, 66, 3),
-)
-ds_plot.plot.contour(
-    ax=ax,
-    transform=ccrs.PlateCarree(),
-    levels=[24],
-    colors="r",
-)
-plt.show()
-# %%
+# %% calculate contourlength 24 mm
+def contour_length_for_time(t):
+    # Helper for parallel execution
+    return int(get_contour_length(ds["prw"].sel(time=t), cont=24))
 
+with ProcessPoolExecutor(max_workers=64) as executor:
+    contour_lenghts = list(tqdm(executor.map(contour_length_for_time, ds["time"].values), total=len(ds["time"])))
 
-def plot_prw_field(ds):
-    ds_plot = ds.sel(lon=slice(0, 360), lat=slice(-50, 50)).isel(
-        lon=slice(None, None, 10), lat=slice(None, None, 10)
-    )
-    pad = 2  # for a window of 5, pad by 2 on each side
-    ds_plot = ds_plot.pad(lon=(pad, pad), mode="wrap")
-    ds_plot = ds_plot.rolling(lon=5, lat=5, center=True).mean()
-
-    fig, ax = plt.subplots(
-        figsize=(10, 3), subplot_kw={"projection": ccrs.PlateCarree()}
-    )
-    ds_plot.plot.contourf(
-        ax=ax,
-        transform=ccrs.PlateCarree(),
-        levels=np.arange(15, 66, 3),
-    )
-    ds_plot.plot.contour(
-        ax=ax,
-        transform=ccrs.PlateCarree(),
-        levels=[24],
-        colors="r",
-    )
-    gl = ax.gridlines(draw_labels=True, linewidth=0)
-    gl.top_labels = False
-    gl.right_labels = False
-    return fig, ax
-
-
-def haversine(lon1, lat1, lon2, lat2):
-    # Convert degrees to radians
-    lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
-    dlon = lon2 - lon1
-    dlat = lat2 - lat1
-    a = np.sin(dlat / 2) ** 2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon / 2) ** 2
-    c = 2 * np.arcsin(np.sqrt(a))
-    R = 6371.0  # Earth radius in km
-    return R * c
-
-
-def contour(ds):
-    pad = 2
-    ds_plot = (
-        ds.sel(lat=slice(-50, 50))
-        .isel(lon=slice(None, None, 10), lat=slice(None, None, 10))
-        .pad(lon=(pad, pad), mode="wrap")
-        .rolling(lon=5, lat=5, center=True)
-        .mean()
-    )
-    fig, ax = plt.subplots(
-        figsize=(10, 5), subplot_kw={"projection": ccrs.PlateCarree()}
-    )
-    contour = ds_plot.plot.contour(
-        ax=ax,
-        transform=ccrs.PlateCarree(),
-        levels=[24],
-        colors="r",
-    )
-    plt.close(fig)
-    # plt.show()
-    return contour
-
-
-def get_contour_length(ds):
-    cont = contour(ds)
-    total_length = 0.0
-    # Iterate over all segments at the first (and only) level
-    for segment in cont.allsegs[0]:
-        lon = segment[:, 0]
-        lat = segment[:, 1]
-        segment_lengths = haversine(lon[:-1], lat[:-1], lon[1:], lat[1:])
-        total_length += segment_lengths.sum()
-    return total_length
-
-
-# %% calculate contourlength
-contour_lenghts = []
-for t in tqdm(ds["time"]):
-    cont_length = get_contour_length(ds["prw"].sel(time=t))
-    contour_lenghts.append(int(cont_length))
-
-contour_lenghts = xr.DataArray(
+contour_lenghts_24 = xr.DataArray(
     contour_lenghts / np.max(contour_lenghts),
     dims=["time"],
     coords={"time": ds["prw"].time},
     attrs={"long_name": "Contour length of 24 mm PRW"},
 )
 
-# %% plot contour length
+# %% plot contour length 30mm
 fig, ax = plt.subplots(figsize=(10, 5))
-contour_lenghts.plot(ax=ax, color="k", label="Contour length")
+contour_lenghts_30.plot(ax=ax, color="k", label="Contour length")
 ax.set_xlabel("Time")
-ax.set_ylabel("Normalized 24 mm contour length")
+ax.set_ylabel("Normalized 30 mm contour length")
+ax.spines["top"].set_visible(False)
+ax.spines["right"].set_visible(False)
+ax.axvline(x=dt.datetime.strptime("1979-07-02", "%Y-%m-%d"), color="grey")
+ax.axvline(x=dt.datetime.strptime("1979-07-17", "%Y-%m-%d"), color="grey")
+fig.savefig("plots/moist_layers/contour_length_30.png", dpi=300, bbox_inches="tight")
+
+# %% plot contour length 24mm
+fig, ax = plt.subplots(figsize=(10, 5))
+contour_lenghts_24.plot(ax=ax, color="k", label="Contour length")
+ax.set_xlabel("Time")
+ax.set_ylabel("Normalized 30 mm contour length")
 ax.spines["top"].set_visible(False)
 ax.spines["right"].set_visible(False)
 ax.axvline(x=dt.datetime.strptime("1979-07-14", "%Y-%m-%d"), color="grey")
 ax.axvline(x=dt.datetime.strptime("1979-09-23", "%Y-%m-%d"), color="grey")
-fig.savefig("plots/moist_layers/contour_length.png", dpi=300, bbox_inches="tight")
+fig.savefig("plots/moist_layers/contour_length_24.png", dpi=300, bbox_inches="tight")
+
+# %% correlate contour lenths 
+corr = xr.corr(contour_lenghts_24, contour_lenghts_30)
+fig, ax = plt.subplots(figsize=(5, 5))
+ax.scatter(
+    contour_lenghts_24,
+    contour_lenghts_30,
+    color="k",
+    s=5,
+)
+ax.set_title(f"Correlation: {corr.values:.2f}")
+ax.set_xlabel("Normalized 24 mm contour length")
+ax.set_ylabel("Normalized 30 mm contour length")
+ax.spines["top"].set_visible(False)
+ax.spines["right"].set_visible(False)
+fig.savefig("plots/moist_layers/contour_length_correlation_trop.png", dpi=300, bbox_inches="tight")
 
 # %%
-fig, ax = plot_prw_field(ds["prw"].sel(time="1979-07-14"))
-fig.savefig("plots/moist_layers/prw_field_1979-07-14.png", dpi=300, bbox_inches="tight")
-fig, ax = plot_prw_field(ds["prw"].sel(time="1979-09-23"))
-fig.savefig("plots/moist_layers/prw_field_1979-09-23.png", dpi=300, bbox_inches="tight")
+fig, ax = plot_prw_field(ds["prw"].sel(time="1979-07-02"), 30)
+fig.savefig("plots/moist_layers/prw_field_1979-07-02.png", dpi=300, bbox_inches="tight")
+fig, ax = plot_prw_field(ds["prw"].sel(time="1979-07-17"), 30)
+fig.savefig("plots/moist_layers/prw_field_1979-07-17.png", dpi=300, bbox_inches="tight")
 
+
+# %%
 
 # %%
